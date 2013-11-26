d39e20bcaf877fdad8e8cc63abc99adbb9106afa
Hack Reactor Students <noahjcz@gmail.com>
2013-11-25 17:31:40 -0800

    added room functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,8 +11,8 @@ var getQueryVariable = function(variable){
   return(false);
 };
 
-
-var friends = [];
+var startingLocation = ""+window.location;
+var friends = {};
 var roomname = getQueryVariable("room");
 var userName = getQueryVariable("username");
 var newdiv;
@@ -44,13 +44,20 @@ getMessages(roomname);
 setInterval(function(){getMessages(roomname);},3000);
 
 var msgRenderHtml = function(msg){
-  $newLink = $('<a href="#"></a>');
+  $newLink = $('<a class="username" href="#"></a>');
   $newLink.text(msg.username+": ");
+  $newLink2 = $('<a class="roomname" href="#"></a>');
+  $newLink2.text(" Room: " + msg.roomname);
   $newdiv = $('<div class="message"></div>').text(msg.text);
-  $newdiv.addClass(msg.username);
-  $newdiv.addClass(msg.roomname);
+  $newLink.attr("username",msg.username);
+  $newLink2.attr("roomname",msg.roomname);
   $newdiv.prepend($newLink);
-  $('.mainMessages').prepend($newdiv);
+  $newdiv.append($newLink2);
+  if (lastMsgTime){
+    $('.mainMessages').prepend($newdiv);
+  } else{
+    $('.mainMessages').append($newdiv);
+  }
 };
 
 
@@ -65,10 +72,27 @@ $(document).ready(function(){
   });
 
   $('.makeRoom').click(function(){
-    window.location += '&room=' + prompt('what would you like to call your new room?');
+    window.location += '&room=' + encodeURI( prompt('what would you like to call your new room?') );
+  });
+  $('.mainMessages').on('click','.message .username',function(){
+    var username = $(this).attr("username");
+    friends[username]=true;
+    styleFriends();
+    return false;
+  });
+
+  $('.mainMessages').on('click','.message .roomname',function(){
+    var roomname = $(this).attr("roomname");
+    window.location += '&room=' + roomname;
+    return false;
   });
 
 });
+var styleFriends = function(){
+  for (var friend in friends){
+    $('[username="' + friend + '"]').parent().css({'font-weight':'600'});
+  }
+};
 var postMessage = function(){
   var message = {
     'username': userName,
